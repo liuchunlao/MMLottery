ae60279729c5d7b273ff676ba3184e24f9f2ccbb
刘春牢 <liuchunlao@qq.com>
2016-05-25 22:36:42 +0800

    17-点击按钮切换状态
    1.监听底部按钮的点击事件
    2.修改按钮状态，并保证只有一个被选中
    - 定义属性，保存被选中的按钮
    - 将保存到恢复
    - 将点击的按钮select状态设置为yes
    - 将点击的按钮保存起来

--- shczLottery/Classes/Main[主体]/Views/MMBottomView.m
+++ shczLottery/Classes/Main[主体]/Views/MMBottomView.m
@@ -8,6 +8,14 @@
 
 #import "MMBottomView.h"
 
+@interface MMBottomView ()
+
+/** 保存选中的按钮 */
+@property (nonatomic, weak) UIButton *selectBtn;
+
+@end
+
+
 @implementation MMBottomView
 
 #pragma mark - 创建按钮，设置图片，添加到自己身上
@@ -23,6 +31,28 @@
     
     // 3.添加按钮
     [self addSubview:btn];
+    
+    // MARK: - 2.监听按钮点击事件
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+#pragma mark - 当点击底部工具条上的按钮时调用
+- (void)btnClick:(UIButton *)sender {
+
+    // MARK: - 1.修改按钮的状态
+    // 1.将之前选中的恢复
+    self.selectBtn.selected = NO;
+    
+    // 2.将现在点击的选中
+    sender.selected = YES;
+    
+    // 3.将现在选中的按钮存起来
+    self.selectBtn = sender;
+    
+    // MARK: - 2.切换标签控制器的子控制器
+
+    
+    
 }
 
 
