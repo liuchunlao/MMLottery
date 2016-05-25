6ae509f9dab08a2d26f94eeef7de320011eb4764
刘春牢 <liuchunlao@qq.com>
2016-05-25 23:25:21 +0800

    21-设置导航栏的背景&标题
    1.在自定义导航控制器中重写intialize方法
    - 获取导航栏的外观代理对象，通过appearance方法获取
    - 统一设置导航栏的背景及标题颜色

--- shczLottery/Classes/Main[主体]/Controllers/MMNavigationController.m
+++ shczLottery/Classes/Main[主体]/Controllers/MMNavigationController.m
@@ -14,9 +14,43 @@
 
 @implementation MMNavigationController
 
+#pragma mark - 系统在第一次初始化这个类的时候，调用一次，以后都不会调用了
++ (void)initialize {
+
+    MMLog(@"initialize方法被调用了！");
+    
+    // MARK: - 1.通过appearance方法获取导航条的外观代理对象，然后进行设置
+    /**
+     外观代理对象的作用：只要设置了外观，如果不再进行修改，将来所有这种类型的控件对象，显示出来的效果都是一样的！
+     */
+    // 1.获取外观代理对象
+    UINavigationBar *navBar = [UINavigationBar appearance];
+
+    // 2.设置导航栏红色背景图
+    [navBar setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
+    
+    // 3.设置导航啦标题的显示效果
+    [navBar setTitleTextAttributes:@{
+                                     NSForegroundColorAttributeName : [UIColor whiteColor]
+                                     }];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // MARK: - 1.统一设置导航栏的背景
+    // metricsDefault代表在任何情况下都显示红色的背景图
+    // 分为横屏，竖屏及导航栏有prompt标题等几种场景！
+    
+    /**
+     ps: 直接写在这里，没有问题，可以实现给导航栏都设置红色背景，只不过就是第一次切换其他子控制器的时候，都会创建导航控制器对象，然后分别走一次viewDidLoad方法，也就说会调用5次设置背景图片的操作！
+     
+     优化: 任何一个类里面都有一个intialize类方法！是在系统初始化此类的时候调用的方法!并且只调用一次！
+     */
+    MMLog(@"导航控制器视图加载完成");
+//    [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
+    
 }
 
 - (void)didReceiveMemoryWarning {
